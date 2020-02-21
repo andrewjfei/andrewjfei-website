55e812b7a696ea74b64cc69fe35a3e94505639f8
andrewjfei <andrewjfei@gmail.com>
2020-02-21 20:06:07 +1300

    max-width 1385px is responsive now (only home screen)

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-favourites',
@@ -9,7 +10,7 @@ export class FavouritesComponent implements OnInit {
 
   favourites = [];
 
-  constructor() {
+  constructor(private router: Router) {
     for (let i = 0; i < 9; i++) {
 
       this.favourites.push({name: 'Mercer Bay Loop', country: 'New Zealand', url: '../../assets/images/mercer-bay-loop.jpg'});
@@ -19,6 +20,16 @@ export class FavouritesComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  goToPage(url) {
+    this.router.navigate([url]).then( (e) => {
+      if (e) {
+        console.log('Navigation is successful!');
+      } else {
+        console.log('Navigation has failed!');
+      }
+    });
   }
 
 }
